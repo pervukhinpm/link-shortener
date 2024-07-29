7cf5540960d10f9e094719829731513144581b9b
Petr Pervukhin <pperv@mail.ru>
2024-07-29 14:32:26 +0300

    test

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -7,8 +7,8 @@ import (
 
 func (h *ShortenerHandler) useRoutes() *chi.Mux {
 	r := chi.NewRouter()
-	r.HandleFunc("POST /", middleware.RequestLogger(middleware.GzipMiddleware(h.CreateShortenerURL)))
-	r.HandleFunc("GET /{id}", middleware.RequestLogger(middleware.GzipMiddleware(h.GetShortenerURL)))
-	r.HandleFunc("POST /api/shorten", middleware.RequestLogger(middleware.GzipMiddleware(h.CreateJSONShortenerURL)))
+	r.Post("/", middleware.RequestLogger(middleware.GzipMiddleware(h.CreateShortenerURL)))
+	r.Get("/{id}", middleware.RequestLogger(h.GetShortenerURL))
+	r.Post("/api/shorten", middleware.RequestLogger(middleware.GzipMiddleware(h.CreateJSONShortenerURL)))
 	return r
 }
